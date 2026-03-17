6e025ae0ce21f6fd6b14a28cec67e57828cde2ef
Matthew Fan <kungfumo@gmail.com>
2026-03-16 22:11:46 -0500

    add telescope buffers picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,11 +99,23 @@ require('lazy').setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+			require('telescope').setup {
+				pickers = {
+					buffers = {
+						show_all_buffers = true,
+						sort_lastused = true,
+						mappings = {
+							n = { ['D'] = 'delete_buffer' }
+						}
+					}
+				}
+			}
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = '[p]roject [f]iles' })
 			vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = '[p]roject [s]earch' })
 			vim.keymap.set("n", "<leader>pd", builtin.diagnostics, { desc = '[p]roject [d]iagnostics' })
-
+			vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = '[p]roject [b]uffers' })
 		end
 	},
 
